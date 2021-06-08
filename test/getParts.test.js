d99b5831f619492feea00dd025b5642ec172e149
Gangmin-dev <vinu10045@snu.ac.kr>
2021-06-08 17:55:21 +0900

    Move mysql config, createTables, insertGivenData to test/lib/mysql.js

--- test/getParts.test.js
+++ test/getParts.test.js
@@ -1,18 +1,7 @@
 const { expect, test, beforeAll, afterAll } = require("@jest/globals");
 const lambdaEventMock = require("lambda-event-mock");
-const fs = require("fs");
 const path = require("path");
 const { DockerComposeEnvironment } = require("testcontainers");
-const mysql = require("serverless-mysql")({
-  library: require("mysql2"),
-  config: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-  },
-});
 
 const getParts = require("../src/getParts");
 
@@ -89,31 +78,3 @@ describe("getParts", () => {
     await environment.down();
   }, 20000);
 });
-
-async function createTables(tables) {
-  const sqlPath = path.join(__dirname, "../sql/src");
-  let sql = "";
-
-  for (let tableName of tables) {
-    sql += fs.readFileSync(path.join(sqlPath, `/${tableName}.sql`)).toString();
-  }
-
-  mysql.config({ multipleStatements: true });
-  await mysql.query(sql).catch(console.log);
-  mysql.config({ multipleStatements: false });
-  mysql.end();
-}
-
-async function insertGivenData(tables) {
-  const sqlPath = path.join(__dirname, "../sql/testdata");
-  let sql = "";
-
-  for (let tableName of tables) {
-    sql += fs.readFileSync(path.join(sqlPath, `/${tableName}.sql`)).toString();
-  }
-
-  mysql.config({ multipleStatements: true });
-  await mysql.query(sql).catch(console.log);
-  mysql.config({ multipleStatements: false });
-  mysql.end();
-}
---
+++ test/lib/mysql.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const mysql = require("serverless-mysql")({
+  library: require("mysql2"),
+  config: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+  },
+});
+
+async function createTables(tables) {
+  const sqlPath = path.join(__dirname, "../sql/src");
+  let sql = "";
+
+  for (let tableName of tables) {
+    sql += fs.readFileSync(path.join(sqlPath, `/${tableName}.sql`)).toString();
+  }
+
+  mysql.config({ multipleStatements: true });
+  await mysql.query(sql).catch(console.log);
+  mysql.config({ multipleStatements: false });
+  mysql.end();
+}
+
+async function insertGivenData(tables) {
+  const sqlPath = path.join(__dirname, "../sql/testdata");
+  let sql = "";
+
+  for (let tableName of tables) {
+    sql += fs.readFileSync(path.join(sqlPath, `/${tableName}.sql`)).toString();
+  }
+
+  mysql.config({ multipleStatements: true });
+  await mysql.query(sql).catch(console.log);
+  mysql.config({ multipleStatements: false });
+  mysql.end();
+}
