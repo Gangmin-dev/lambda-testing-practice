c2e8d1a5be8688852359e191835e197a1deb7d67
Gangmin-dev <vinu10045@snu.ac.kr>
2021-06-14 05:43:29 +0900

    Add ls and cd script for debugging CD

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,10 @@ npm install -g --silent --no-progress serverless
 npm install --silent --no-progress serverless-offline
 echo $CODEBUILD_SRC_DIR/target/$env
 ls -al
+cd target
+ls -al
+cd dev
+ls -al
+cd ../..
 serverless deploy --stage $env --package \
 target/$env --region ap-northeast-2
