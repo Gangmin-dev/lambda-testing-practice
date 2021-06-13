f03bdd7f579b4c1daee2acaf1e94f9c08bc1ac57
Gangmin-dev <vinu10045@snu.ac.kr>
2021-06-14 05:18:48 +0900

    Add echo directory path to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 npm install --silent --no-progress -g npm
 npm install -g --silent --no-progress serverless
 npm install --silent --no-progress serverless-offline
+echo $CODEBUILD_SRC_DIR/target/$env
 serverless deploy --stage $env --package \
 $CODEBUILD_SRC_DIR/target/$env --region ap-northeast-2
