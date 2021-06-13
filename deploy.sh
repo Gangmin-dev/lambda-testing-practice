7272e4f7d50c0a7465b10c8f3fcbeaf937bacd8e
Gangmin-dev <vinu10045@snu.ac.kr>
2021-06-14 05:27:26 +0900

    Remove $CODEBUILD_SRC_DIR from serverless deploy script in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ npm install -g --silent --no-progress serverless
 npm install --silent --no-progress serverless-offline
 echo $CODEBUILD_SRC_DIR/target/$env
 serverless deploy --stage $env --package \
-$CODEBUILD_SRC_DIR/target/$env --region ap-northeast-2
+target/$env --region ap-northeast-2
