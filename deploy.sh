9f82ad5ef0d2ae7ea9f54e93185f4869aa6ed0eb
Gangmin-dev <vinu10045@snu.ac.kr>
2021-06-14 06:18:44 +0900

    Add -v command to deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ ls -al
 cd dev
 ls -al
 cd ../..
-serverless deploy --stage $env --package target/$env --region ap-northeast-2
+serverless deploy --stage $env --package target/$env --region ap-northeast-2 -v
