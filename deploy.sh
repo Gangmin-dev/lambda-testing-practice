2126cb0d9475a32c17cf2503b101ffacc0fd896e
Gangmin-dev <vinu10045@snu.ac.kr>
2021-06-14 05:04:29 +0900

    Fix -region to --region in deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ npm install --silent --no-progress -g npm
 npm install -g --silent --no-progress serverless
 npm install --silent --no-progress serverless-offline
 serverless deploy --stage $env --package \
-$CODEBUILD_SRC_DIR/target/$env -region ap-northeast-2
+$CODEBUILD_SRC_DIR/target/$env --region ap-northeast-2
