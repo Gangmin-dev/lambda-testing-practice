8d89ed3511b9d0b6fda8a3d3a2d258063ff85cc1
Gangmin-dev <vinu10045@snu.ac.kr>
2021-06-14 05:34:41 +0900

    Add ls -al to deploy.sh for debug

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ npm install --silent --no-progress -g npm
 npm install -g --silent --no-progress serverless
 npm install --silent --no-progress serverless-offline
 echo $CODEBUILD_SRC_DIR/target/$env
+ls -al
 serverless deploy --stage $env --package \
 target/$env --region ap-northeast-2
